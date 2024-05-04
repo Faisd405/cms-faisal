e5eeb8d9bb9442821640278517f3375129817e8b
faisd405 <faisd405@gmail.com>
2024-05-04 20:46:21 +0700

    feat: Add Models ContentType, Page Content, Datamaster, and Morph Data

---
+++ app/Models/ContentType/ContentTypeField.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\ContentType;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContentTypeField extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Datamaster/Localization.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Datamaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Localization extends Model
+{
+    use HasFactory;
+}
