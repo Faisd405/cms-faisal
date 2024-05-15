6aafaa8e2b323a6226f15d5457e28200963b5196
faisd405 <faisd405@gmail.com>
2024-05-15 21:55:33 +0700

    refactor: Change V Model Strategy

--- resources/js/Components/Partials/Page/FormContent.vue
+++ resources/js/Components/Partials/Page/FormContent.vue
@@ -1,42 +1,3 @@
-<template>
-    <div
-        class="mb-4 bg-white px-4 py-5 shadow sm:rounded-tl-md sm:rounded-tr-md sm:p-6"
-    >
-        <div class="mb-2 border-b pb-2 font-semibold">Page Content</div>
-        <div class="grid grid-cols-6 gap-6">
-            <div
-                v-for="(itemField, key) in contentTypeFields"
-                :key="key"
-                class="col-span-6 sm:col-span-4"
-            >
-                <InputLabel :for="itemField.name" :value="itemField.label" />
-                <TextareaInput
-                    v-if="itemField.type === 'textarea'"
-                    v-model="form[itemField.name].value"
-                    class="mt-1 block w-full"
-                />
-                <TextInput
-                    v-else
-                    v-model="form[itemField.name].value"
-                    :type="itemField.type"
-                    class="mt-1 block w-full"
-                />
-            </div>
-        </div>
-        <div
-            class="mt-2 flex justify-end border-t-2 px-4 pt-2 sm:mt-4 sm:rounded-tl-md sm:rounded-tr-md sm:px-6 sm:pt-4"
-        >
-            <PrimaryButton
-                :class="{ 'opacity-25': isProcessingSubmit }"
-                :disabled="isProcessingSubmit"
-                @click="submitData"
-            >
-                Update Content
-            </PrimaryButton>
-        </div>
-    </div>
-</template>
-
 <script setup>
 import { onBeforeMount, ref } from 'vue'
 import TextareaInput from '@/Components/Form/TextareaInput.vue'
@@ -75,23 +36,64 @@ function submitData() {
         })
 }
 
-onBeforeMount(() => {
-    contentTypeFields.value = props.item.content_type.fields
+const initializeForm = () => {
+    contentTypeFields.value = props.item.content_type.fields || []
 
-    contentTypeFields.value.forEach((item) => {
-        form.value[item.name] = {
+    contentTypeFields.value.forEach((field) => {
+        form.value[field.name] = {
             value: '',
             page_id: props.item.id,
-            content_type_field_id: item.id
+            content_type_field_id: field.id
         }
     })
 
-    if (props.item.page_contents && props.item.page_contents.length > 0) {
-        props.item.page_contents.forEach((item) => {
-            if (form.value[item.content_type_field.name]) {
-                form.value[item.content_type_field.name].value = item.value
-            }
-        })
-    }
+    props.item.page_contents?.forEach((item) => {
+        if (form.value[item.content_type_field.name]) {
+            form.value[item.content_type_field.name].value = item.value
+        }
+    })
+}
+
+onBeforeMount(() => {
+    initializeForm()
 })
 </script>
+
+<template>
+    <div
+        class="mb-4 bg-white px-4 py-5 shadow sm:rounded-tl-md sm:rounded-tr-md sm:p-6"
+    >
+        <div class="mb-2 border-b pb-2 font-semibold">Page Content</div>
+        <div class="grid grid-cols-6 gap-6">
+            <div
+                v-for="(itemField, key) in contentTypeFields"
+                :key="key"
+                class="col-span-6 sm:col-span-4"
+            >
+                <InputLabel :for="itemField.name" :value="itemField.label" />
+                <TextareaInput
+                    v-if="itemField.type === 'textarea'"
+                    v-model="form[itemField['name']].value"
+                    class="mt-1 block w-full"
+                />
+                <TextInput
+                    v-else
+                    v-model="form[itemField['name']].value"
+                    :type="itemField.type"
+                    class="mt-1 block w-full"
+                />
+            </div>
+        </div>
+        <div
+            class="mt-2 flex justify-end border-t-2 px-4 pt-2 sm:mt-4 sm:rounded-tl-md sm:rounded-tr-md sm:px-6 sm:pt-4"
+        >
+            <PrimaryButton
+                :class="{ 'opacity-25': isProcessingSubmit }"
+                :disabled="isProcessingSubmit"
+                @click="submitData"
+            >
+                Update Content
+            </PrimaryButton>
+        </div>
+    </div>
+</template>
